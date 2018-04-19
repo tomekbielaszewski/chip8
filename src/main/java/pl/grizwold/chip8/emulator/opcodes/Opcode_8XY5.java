074b0f5be75915ccd4b793428e447cf57476cfec
tbielaszewski <tomekbielaszewski@gmail.com>
2018-04-19 23:04:35 +0200

    opcode - subtract instruction

---
+++ src/main/java/pl/grizwold/chip8/emulator/opcodes/Opcode_8XY5.java
@@ -0,0 +1,24 @@
+package pl.grizwold.chip8.emulator.opcodes;
+
+import pl.grizwold.chip8.emulator.VirtualMachine;
+
+public class Opcode_8XY5 implements Opcode {
+    @Override
+    public boolean accept(short code) {
+        return (code & 0xF00F) == 0x8005;
+    }
+
+    @Override
+    public void execute(short code, VirtualMachine vm) {
+        short x = (short) ((code & 0x0F00) >>> (8));
+        short y = (short) ((code & 0x00F0) >>> (4));
+
+        if(vm.V[x] < vm.V[y]) {
+            vm.V[15] = 1;
+        } else {
+            vm.V[15] = 0;
+        }
+
+        vm.V[x] = (byte) (vm.V[x] - vm.V[y]);
+    }
+}
