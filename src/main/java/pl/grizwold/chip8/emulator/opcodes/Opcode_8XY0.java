ef88d79fb60c5210e4d29dcfc227cb105c633a22
tbielaszewski <tomekbielaszewski@gmail.com>
2018-04-19 23:27:38 +0200

    opcode - move instruction - tests

--- src/main/java/pl/grizwold/chip8/emulator/opcodes/Opcode_8XY0.java
+++ src/main/java/pl/grizwold/chip8/emulator/opcodes/Opcode_8XY0.java
@@ -10,8 +10,8 @@ public class Opcode_8XY0 implements Opcode {
 
     @Override
     public void execute(short code, VirtualMachine vm) {
-        short x = (short) ((code & 0x0F00) >>> (8*2));
-        short y = (short) ((code & 0x00F0) >>> (8));
+        short x = (short) ((code & 0x0F00) >>> (8));
+        short y = (short) ((code & 0x00F0) >>> (4));
 
         vm.V[x] = vm.V[y];
     }
---
+++ src/test/java/pl/grizwold/chip8/emulator/opcodes/Opcode_8XY0Test.java
@@ -0,0 +1,33 @@
+package pl.grizwold.chip8.emulator.opcodes;
+
+import org.junit.Test;
+import pl.grizwold.chip8.emulator.VirtualMachine;
+
+import static org.junit.Assert.*;
+
+public class Opcode_8XY0Test {
+    private final VirtualMachine vm = new VirtualMachine();
+    private final Opcode opcode = new Opcode_8XY0();
+
+    @Test
+    public void shouldAcceptCodeWizMask8007() throws Exception {
+        short code = (short) 0x8aa0;
+
+        assertTrue(opcode.accept(code));
+    }
+
+    @Test
+    public void shouldResultWithAllSetBits() throws Exception {
+        short code = (short) 0x8120;
+        byte x = 0b00000000;
+        byte y = 0b00110010;
+        vm.V[1] = x;
+        vm.V[2] = y;
+
+        opcode.execute(code, vm);
+
+        assertTrue(vm.V[1] == y);
+        assertTrue(vm.V[2] == y);
+    }
+
+}
