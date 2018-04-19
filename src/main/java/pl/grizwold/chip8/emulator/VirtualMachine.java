880328623662850a4f8e7e1b3ccfc1e46736a902
tbielaszewski <tomekbielaszewski@gmail.com>
2018-04-19 22:57:56 +0200

    VM configuration according to schematics

---
+++ src/main/java/pl/grizwold/chip8/emulator/VirtualMachine.java
@@ -0,0 +1,20 @@
+package pl.grizwold.chip8.emulator;
+
+public class VirtualMachine {
+    private final short PROGRAM_START_ADDRESS = 0x200;
+
+    public byte[] memory = new byte[4096];
+    public byte[] V = new byte[16]; //VX: V0, ..., VF
+
+    public short I = 0; //memory program pointer; index register
+    public short PC = 0; //program counter
+
+    public Stack stack = new Stack();
+
+    public byte delayTimer = 0;
+    public byte soundTimer = 0;
+
+    public boolean[] screen = new boolean[64*32];
+
+
+}
