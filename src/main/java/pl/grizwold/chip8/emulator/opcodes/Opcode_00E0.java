6dcc2866a854c707362bfeb7baa63167c4d1fcb5
tbielaszewski <tomekbielaszewski@gmail.com>
2018-04-19 22:58:49 +0200

    opcode - clr display

---
+++ src/main/java/pl/grizwold/chip8/emulator/opcodes/Opcode_00E0.java
@@ -0,0 +1,18 @@
+package pl.grizwold.chip8.emulator.opcodes;
+
+import pl.grizwold.chip8.emulator.VirtualMachine;
+
+public class Opcode_00E0 implements Opcode {
+
+    @Override
+    public boolean accept(short code) {
+        return code == 0x00e0;
+    }
+
+    @Override
+    public void execute(short code, VirtualMachine vm) {
+        for (int i = 0; i < vm.screen.length; i++) {
+            vm.screen[i] = false;
+        }
+    }
+}
