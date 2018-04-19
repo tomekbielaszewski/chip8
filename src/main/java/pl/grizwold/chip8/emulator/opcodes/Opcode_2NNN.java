3e7e3e3e2ac494788264c104cdaa5c31b5a30025
tbielaszewski <tomekbielaszewski@gmail.com>
2018-04-19 23:00:15 +0200

    opcode - call instruction

---
+++ src/main/java/pl/grizwold/chip8/emulator/opcodes/Opcode_2NNN.java
@@ -0,0 +1,16 @@
+package pl.grizwold.chip8.emulator.opcodes;
+
+import pl.grizwold.chip8.emulator.VirtualMachine;
+
+public class Opcode_2NNN implements Opcode {
+    @Override
+    public boolean accept(short code) {
+        return (code & 0xF000) == 0x2000;
+    }
+
+    @Override
+    public void execute(short code, VirtualMachine vm) {
+        vm.stack.push(vm.PC);
+        vm.PC = (short) (0x0FFF & code);
+    }
+}
