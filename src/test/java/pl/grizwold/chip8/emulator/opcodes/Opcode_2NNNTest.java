7833d53263196e5c95522b7828cb193f3b5ff21b
tbielaszewski <tbielaszewski@objectivity.co.uk>
2018-04-20 10:35:50 +0200

    opcode - call instruction - tests

---
+++ src/test/java/pl/grizwold/chip8/emulator/opcodes/Opcode_2NNNTest.java
@@ -0,0 +1,33 @@
+package pl.grizwold.chip8.emulator.opcodes;
+
+import org.junit.Test;
+import pl.grizwold.chip8.emulator.VirtualMachine;
+
+import static org.junit.Assert.*;
+
+public class Opcode_2NNNTest {
+    private final VirtualMachine vm = new VirtualMachine();
+    private final Opcode opcode = new Opcode_2NNN();
+
+    @Test
+    public void shouldAcceptCodeWizMask8007() throws Exception {
+        short code = (short) 0x2abb;
+
+        assertTrue(opcode.accept(code));
+    }
+
+    @Test
+    public void shouldResultWithIncrementedProgramCounter() throws Exception {
+        short code = (short) 0x2aaa;
+        short pcBefore = 0x7fff;
+        vm.PC = pcBefore;
+        short stackPopBefore = vm.stack.pop();
+
+        opcode.execute(code, vm);
+
+        short stackPopAfter = vm.stack.pop();
+        assertTrue(stackPopBefore == 0);
+        assertTrue(stackPopAfter == pcBefore);
+        assertTrue(vm.PC == 0xaaa);
+    }
+}
