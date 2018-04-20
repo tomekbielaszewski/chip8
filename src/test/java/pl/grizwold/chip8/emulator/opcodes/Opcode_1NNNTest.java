f0f51d1b24572d7197e3c2293f3415f84bbca923
tbielaszewski <tbielaszewski@objectivity.co.uk>
2018-04-20 10:37:38 +0200

    opcode - jump instruction - tests

---
+++ src/test/java/pl/grizwold/chip8/emulator/opcodes/Opcode_1NNNTest.java
@@ -0,0 +1,30 @@
+package pl.grizwold.chip8.emulator.opcodes;
+
+import org.junit.Test;
+import pl.grizwold.chip8.emulator.VirtualMachine;
+
+import static org.junit.Assert.*;
+
+public class Opcode_1NNNTest {
+    private final VirtualMachine vm = new VirtualMachine();
+    private final Opcode opcode = new Opcode_1NNN();
+
+    @Test
+    public void shouldAcceptCodeWizMask8007() throws Exception {
+        short code = (short) 0x1abb;
+
+        assertTrue(opcode.accept(code));
+    }
+
+    @Test
+    public void shouldResultWithIncrementedProgramCounter() throws Exception {
+        short code = (short) 0x1aaa;
+        short pcBefore = 0x7fff;
+        vm.PC = pcBefore;
+
+        opcode.execute(code, vm);
+
+        assertTrue(vm.PC == 0xaaa);
+        assertTrue(vm.PC != pcBefore);
+    }
+}
