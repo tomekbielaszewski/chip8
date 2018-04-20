6a26c2904fc97b5f338a523a03b11eec09bd002c
tbielaszewski <tomekbielaszewski@gmail.com>
2018-04-20 20:46:28 +0200

    opcode - keyboard keypress check - impl & tests

---
+++ src/main/java/pl/grizwold/chip8/emulator/opcodes/Opcode_EKA1.java
@@ -0,0 +1,19 @@
+package pl.grizwold.chip8.emulator.opcodes;
+
+import pl.grizwold.chip8.emulator.VirtualMachine;
+
+public class Opcode_EKA1 implements Opcode {
+    @Override
+    public boolean accept(short code) {
+        return (code & 0xF0FF) == 0xE0A1;
+    }
+
+    @Override
+    public void execute(short code, VirtualMachine vm) {
+        short x = (short) ((code & 0x0F00) >>> 8);
+
+        if((vm.keyboard & (1 << vm.V[x])) == 0) {
+            vm.PC += 2;
+        }
+    }
+}
---
+++ src/test/java/pl/grizwold/chip8/emulator/opcodes/Opcode_EKA1Test.java
@@ -0,0 +1,70 @@
+package pl.grizwold.chip8.emulator.opcodes;
+
+import org.junit.Test;
+import pl.grizwold.chip8.emulator.VirtualMachine;
+
+import static org.junit.Assert.*;
+
+public class Opcode_EKA1Test {
+    private final VirtualMachine vm = new VirtualMachine();
+    private final Opcode opcode = new Opcode_EKA1();
+
+    @Test
+    public void shouldAcceptCode() throws Exception {
+        short code = (short) 0xE1A1;
+
+        assertTrue(opcode.accept(code));
+    }
+
+    @Test
+    public void shouldPCStayTheSameWhenKeyCodeInRegisterCorrespondsToKeyboard() throws Exception {
+        short code = (short) 0xE1A1;
+        short pcBefore = 100;
+        vm.V[1] = 5;
+        vm.PC = pcBefore;
+        vm.keyboard = 0b00100000;
+
+        opcode.execute(code, vm);
+
+        assertTrue(vm.PC == pcBefore);
+    }
+
+    @Test
+    public void shouldPCStayTheSameWhenAllKeysOnKeyboardArePressed() throws Exception {
+        short code = (short) 0xE1A1;
+        short pcBefore = 100;
+        vm.V[1] = 5;
+        vm.PC = pcBefore;
+        vm.keyboard = 0b11111111;
+
+        opcode.execute(code, vm);
+
+        assertTrue(vm.PC == pcBefore);
+    }
+
+    @Test
+    public void shouldIncrementPCWhenKeyCodeInRegisterIsCloseToKeyboardFlagButNotExact() throws Exception {
+        short code = (short) 0xE1A1;
+        short pcBefore = 100;
+        vm.V[1] = 5;
+        vm.PC = pcBefore;
+        vm.keyboard = 0b01010000;
+
+        opcode.execute(code, vm);
+
+        assertTrue(vm.PC == pcBefore + 2);
+    }
+
+    @Test
+    public void shouldIncrementPCWhenNoKeysPressed() throws Exception {
+        short code = (short) 0xE1A1;
+        short pcBefore = 100;
+        vm.V[1] = 5;
+        vm.PC = pcBefore;
+        vm.keyboard = 0b00000000;
+
+        opcode.execute(code, vm);
+
+        assertTrue(vm.PC == pcBefore + 2);
+    }
+}
