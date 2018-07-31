030fd3ac46827ffe57732dddb9918bae55bb0327
tbielaszewski <tomekbielaszewski@gmail.com>
2018-07-31 20:50:33 +0200

    opcode - sprite drawing - impl & tests

--- src/main/java/pl/grizwold/chip8/emulator/opcodes/Opcode_DXYN.java
+++ src/main/java/pl/grizwold/chip8/emulator/opcodes/Opcode_DXYN.java
@@ -1,9 +1,6 @@
 package pl.grizwold.chip8.emulator.opcodes;
 
 import pl.grizwold.chip8.emulator.VirtualMachine;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Opcode_DXYN implements Opcode {
     @Override
@@ -13,22 +10,56 @@ public class Opcode_DXYN implements Opcode {
 
     @Override
     public void execute(short code, VirtualMachine vm) {
-        short x = (short) ((code & 0x0F00) >>> 8);
-        short y = (short) ((code & 0x00F0) >>> 4);
+        short _x = (short) ((code & 0x0F00) >>> 8);
+        short _y = (short) ((code & 0x00F0) >>> 4);
         short n = (short) (code & 0x000F);
 
-        byte sprite = loadSprite(n, vm);
+        byte x = vm.V[_x];
+        byte y = vm.V[_y];
+
+        byte[] sprite = loadSprite(n, vm);
         boolean collision = drawSprite(x, y, sprite, vm);
-        vm.V[15] = (byte) (collision ? 1 : 0);
+        vm.V[0xF] = (byte) (collision ? 1 : 0);
     }
 
-    private byte loadSprite(short n, VirtualMachine vm) {
-        throw new NotImplementedException();
+    private byte[] loadSprite(short n, VirtualMachine vm) {
+        byte[] sprite = new byte[n];
+        System.arraycopy(vm.memory, vm.I, sprite, 0, n);
+        return sprite;
     }
 
-    private boolean drawSprite(short x, short y, byte sprite, VirtualMachine vm) {
-        throw new NotImplementedException();
+    private boolean drawSprite(int x, int y, byte[] sprite, VirtualMachine vm) {
+        boolean collision = false;
+
+        for (int spriteNum = 0; spriteNum < sprite.length; spriteNum++) {
+            byte spritePart = sprite[spriteNum];
+            collision |= drawSpritePart(x + (8 * spriteNum), y, spritePart, vm);
+        }
+
+        return collision;
     }
 
+    private boolean drawSpritePart(int x, int y, byte spritePart, VirtualMachine vm) {
+        boolean collision = false;
+        spritePart = reverse(spritePart);
 
+        for (int bitNum = 1; bitNum <= 8; bitNum++) {
+            int _x = x + bitNum - 1;
+            _x = _x >= 64 ? _x - 64 : _x;
+            boolean pixelBefore = vm.screen[_x][y];
+            vm.screen[_x][y] ^= isNthBitOn(spritePart, bitNum);
+
+            collision |= (pixelBefore && !vm.screen[_x][y]);
+        }
+
+        return collision;
+    }
+
+    private boolean isNthBitOn(byte _byte, int bitNum) {
+        return (_byte & (1 << (bitNum - 1))) >= 1;
+    }
+
+    private byte reverse(byte _byte) {
+        return (byte) (Integer.reverse(_byte) >>> (Integer.SIZE - Byte.SIZE));
+    }
 }
--- src/test/java/pl/grizwold/chip8/emulator/opcodes/Opcode_DXYNTest.java
+++ src/test/java/pl/grizwold/chip8/emulator/opcodes/Opcode_DXYNTest.java
@@ -3,7 +3,9 @@ package pl.grizwold.chip8.emulator.opcodes;
 import org.junit.Test;
 import pl.grizwold.chip8.emulator.VirtualMachine;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class Opcode_DXYNTest {
     private final VirtualMachine vm = new VirtualMachine();
@@ -17,8 +19,74 @@ public class Opcode_DXYNTest {
     }
 
     @Test
-    public void shouldExecuteOpcode() throws Exception {
-        short code = (short) 0xdabb;
+    public void shouldDrawSinglePixelInLeftUpperCorner() throws Exception {
+        short code = (short) 0xd451;
+        vm.V[0x4] = 0;
+        vm.V[0x5] = 0;
+        vm.I = 0x201;
+        vm.memory[0x201] = (byte) 0b10000000;
+
         opcode.execute(code, vm);
+
+        assertThat(vm.screen[0][0], is(true));
     }
+
+    @Test
+    public void shouldDrawSinglePixelOn8thPixelToTheRightFromLeftUpperCorner() throws Exception {
+        short code = (short) 0xd451;
+        vm.V[0x4] = 0;
+        vm.V[0x5] = 0;
+        vm.I = 0x201;
+        vm.memory[0x201] = (byte) 0b00000001;
+
+        opcode.execute(code, vm);
+
+        assertThat(vm.screen[7][0], is(true));
+    }
+
+    @Test
+    public void shouldDraw8PixelsOnLeftUpperCorner() throws Exception {
+        short code = (short) 0xd451;
+        vm.V[0x4] = 0;
+        vm.V[0x5] = 0;
+        vm.I = 0x201;
+        vm.memory[0x201] = (byte) 0b11111111;
+
+        opcode.execute(code, vm);
+
+        for (int i = 0; i < 8; i++) {
+            assertTrue(String.format("%d %d was not true", i, 0), vm.screen[i][0]);
+        }
+    }
+
+    @Test
+    public void shouldDraw16PixelsOnLeftUpperCorner() throws Exception {
+        short code = (short) 0xd002;
+        vm.I = 0x201;
+        vm.memory[0x201] = (byte) 0b11111111;
+        vm.memory[0x202] = (byte) 0b11111111;
+
+        opcode.execute(code, vm);
+
+        for (int i = 0; i < 16; i++) {
+            assertTrue(String.format("%d %d was not true", i, 0), vm.screen[i][0]);
+        }
+    }
+
+    @Test
+    public void shouldWrapTheSpriteAroundTheScreen() throws Exception {
+        short code = (short) 0xd451;
+        vm.V[0x4] = 60;
+        vm.V[0x5] = 0;
+        vm.I = 0x201;
+        vm.memory[0x201] = (byte) 0b11111111;
+
+        opcode.execute(code, vm);
+
+        for (int i = 60; i < 68; i++) {
+            int x = i >= 64 ? i - 64 : i;
+            assertTrue(String.format("%d %d was not true", i, 0), vm.screen[x][0]);
+        }
+    }
+
 }
