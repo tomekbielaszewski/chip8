0ab7dce22b970ce6000bd418e8fe2ff89c299b80
tbielaszewski <tomekbielaszewski@gmail.com>
2018-08-01 00:01:38 +0200

    loading game file

--- src/main/java/pl/grizwold/chip8/Chip8EmulatorStarter.java
+++ src/main/java/pl/grizwold/chip8/Chip8EmulatorStarter.java
@@ -1,8 +1,21 @@
 package pl.grizwold.chip8;
 
+import org.apache.commons.io.IOUtils;
+import pl.grizwold.chip8.emulator.VirtualMachine;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Chip8EmulatorStarter {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        InputStream inputStream = Files.newInputStream(Paths.get("C:\\Programming\\Prv\\chip8\\src\\main\\resources\\15PUZZLE.ch8"));
+        byte[] bytes = IOUtils.toByteArray(inputStream);
+
+        VirtualMachine vm = new VirtualMachine(50);
+        vm.start(bytes);
 
     }
 }
