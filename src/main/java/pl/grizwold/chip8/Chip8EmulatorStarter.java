08f6e5d1e0edfcbc6e384ac339274923c8bd6fd2
tbielaszewski <tomekbielaszewski@gmail.com>
2018-08-01 23:20:37 +0200

    javafx canvas introduced for drawing

--- src/main/java/pl/grizwold/chip8/Chip8EmulatorStarter.java
+++ src/main/java/pl/grizwold/chip8/Chip8EmulatorStarter.java
@@ -1,5 +1,11 @@
 package pl.grizwold.chip8;
 
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.stage.Stage;
 import org.apache.commons.io.IOUtils;
 import pl.grizwold.chip8.emulator.VirtualMachine;
 
@@ -7,15 +13,50 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class Chip8EmulatorStarter {
+public class Chip8EmulatorStarter extends Application {
+    private ExecutorService executorService = Executors.newSingleThreadExecutor();
 
     public static void main(String[] args) throws IOException {
-        InputStream inputStream = Files.newInputStream(Paths.get("C:\\Programming\\Prv\\chip8\\src\\main\\resources\\C8PIC.ch8"));
-        byte[] bytes = IOUtils.toByteArray(inputStream);
+        launch(args);
+    }
 
-        VirtualMachine vm = new VirtualMachine(50);
-        vm.start(bytes);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Group root = new Group();
+        Canvas canvas = new Canvas(640, 320);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        root.getChildren().add(canvas);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
 
+        executorService.submit(() -> {
+            VirtualMachine vm = new VirtualMachine(50);
+            try {
+                InputStream inputStream = Files.newInputStream(Paths.get("C:\\Programming\\Prv\\chip8\\src\\main\\resources\\test\\C8PIC.ch8"));
+                byte[] bytes = IOUtils.toByteArray(inputStream);
+                vm.start(bytes, (screen) -> drawShapes(gc, (boolean[][]) screen));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        executorService.shutdownNow();
+    }
+
+    private static void drawShapes(GraphicsContext gc, boolean[][] screen) {
+        for (int x = 0; x < screen.length; x++) {
+            for (int y = 0; y < screen[x].length; y++) {
+                if (screen[x][y]) {
+                    gc.fillRect(x * 10, y * 10, 10, 10);
+                }
+            }
+        }
     }
 }
