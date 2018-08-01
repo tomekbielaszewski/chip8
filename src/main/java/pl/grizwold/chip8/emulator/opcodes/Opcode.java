d6c277c1470f2e3a278d14544c75c6add29de7cc
tbielaszewski <tomekbielaszewski@gmail.com>
2018-08-01 23:19:14 +0200

    corrected opcode logging

--- src/main/java/pl/grizwold/chip8/emulator/opcodes/Opcode.java
+++ src/main/java/pl/grizwold/chip8/emulator/opcodes/Opcode.java
@@ -4,18 +4,19 @@ import pl.grizwold.chip8.emulator.VirtualMachine;
 
 import java.util.Arrays;
 
-import static java.lang.Integer.toHexString;
-
 public interface Opcode {
     int INT_TO_BYTE_MASK = 0xff;
 
     boolean accept(short code);
+
     void execute(short code, VirtualMachine vm);
+
     String getDescription();
+
     String getAsm(short code);
 
     default void execute_(short code, VirtualMachine vm) {
-        System.out.println(String.format("0x%-3X: [%-4X] %-15s\\\\%-115s |  I = %-5X V%s", (vm.PC & 0xFF), (code & 0xFFFF), getAsm(code), getDescription(), vm.I, Arrays.toString(vm.V)));
+        System.out.println(String.format("%-3d: [%-4X] %-15s\\\\ %-115s |  I = %-5X V%s", vm.PC, (code & 0xFFFF), getAsm(code), getDescription(), vm.I, Arrays.toString(vm.V)));
         this.execute(code, vm);
     }
 }
