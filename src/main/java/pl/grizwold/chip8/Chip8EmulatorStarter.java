3d003d267ff610ed406c507267e47a8a9cf902d8
tbielaszewski <tbielaszewski@objectivity.co.uk>
2018-08-09 17:00:26 +0200

    Keyboard support for JavaFX scene

--- src/main/java/pl/grizwold/chip8/Chip8EmulatorStarter.java
+++ src/main/java/pl/grizwold/chip8/Chip8EmulatorStarter.java
@@ -5,6 +5,7 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import org.apache.commons.io.IOUtils;
 import pl.grizwold.chip8.emulator.VirtualMachine;
@@ -15,11 +16,13 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.Supplier;
 
 public class Chip8EmulatorStarter extends Application {
     private ExecutorService executorService = Executors.newSingleThreadExecutor();
 
     public static void main(String[] args) throws IOException {
+        System.setOut(new Logger("out.log", System.out));
         launch(args);
     }
 
@@ -32,12 +35,32 @@ public class Chip8EmulatorStarter extends Application {
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
 
+        boolean[] keys = new boolean[16];
+        KeyCode[] keyCodes = new KeyCode[]{
+                KeyCode.DIGIT1, KeyCode.DIGIT2, KeyCode.DIGIT3, KeyCode.DIGIT4,
+                KeyCode.Q, KeyCode.W, KeyCode.E, KeyCode.R,
+                KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.F,
+                KeyCode.Z, KeyCode.X, KeyCode.C, KeyCode.V
+        };
+
+        primaryStage.getScene().setOnKeyPressed(event -> {
+            for (int i = 0; i < keyCodes.length; i++) {
+                keys[i] = keyCodes[i].equals(event.getCode());
+            }
+        });
+
+        primaryStage.getScene().setOnKeyReleased(event -> {
+            for (int i = 0; i < keys.length; i++) {
+                keys[i] = false;
+            }
+        });
+
         executorService.submit(() -> {
-            VirtualMachine vm = new VirtualMachine(50);
+            VirtualMachine vm = new VirtualMachine(1000);
             try {
-                InputStream inputStream = Files.newInputStream(Paths.get("C:\\Programming\\Prv\\chip8\\src\\main\\resources\\test\\C8PIC.ch8"));
+                InputStream inputStream = Files.newInputStream(Paths.get("C:\\Programming\\Prv\\chip8\\src\\main\\resources\\test\\Keypad Test [Hap, 2006].ch8"));
                 byte[] bytes = IOUtils.toByteArray(inputStream);
-                vm.start(bytes, (screen) -> drawShapes(gc, (boolean[][]) screen));
+                vm.start(bytes, (screen) -> drawShapes(gc, (boolean[][]) screen), keyboardSupplier(keys));
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -51,6 +74,7 @@ public class Chip8EmulatorStarter extends Application {
     }
 
     private static void drawShapes(GraphicsContext gc, boolean[][] screen) {
+        gc.clearRect(0, 0, 640, 480);
         for (int x = 0; x < screen.length; x++) {
             for (int y = 0; y < screen[x].length; y++) {
                 if (screen[x][y]) {
@@ -58,5 +82,17 @@ public class Chip8EmulatorStarter extends Application {
                 }
             }
         }
+    }
+
+    private static Supplier<Short> keyboardSupplier(boolean[] keys) {
+        return () -> {
+            short keyboard = 0;
+            for (int i = 0; i < keys.length; i++) {
+                if (keys[i]) {
+                    keyboard = (short) ((short) (1 << i) & 0xFFFF);
+                }
+            }
+            return keyboard;
+        };
     }
 }
